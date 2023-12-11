fe756c186d12b5cacfea811486fb7a0b10c1be6b
Juan Cotrino <juan.cotrino@outlook.com>
2023-12-10 22:31:03 -0500

    Added new implementations

--- myproject/settings.py
+++ myproject/settings.py
@@ -126,3 +126,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 MEDIA_URL = '/media/'
 
+# Create automatic id fields in tables
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
